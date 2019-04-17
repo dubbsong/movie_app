16e30aabe5481979daa2dd09dbe308ef6f49265d
dubbsong <dubbsong@gmail.com>
2019-04-17 10:39:51 +0900

    Updates

---
+++ precache-manifest.64d9094f44ad75bdd9fa6f88a830f331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "26a03fd14f885581c6cb",
+    "url": "/movie_app/static/js/main.f5bbb8d1.chunk.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "26a03fd14f885581c6cb",
+    "url": "/movie_app/static/css/main.1863885d.chunk.css"
+  },
+  {
+    "revision": "e2145ec2e449e13645288b88b4780005",
+    "url": "/movie_app/index.html"
+  }
+];
